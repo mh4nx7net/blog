c7278f0a8c0663aecfb0f2ea35ea6c58ec930207
mh4nx7net <mh4nx7net@users.noreply.github.com>
2021-03-02 00:16:04 +0000

    deploy: b0f816b64f379feb195460dbecdf22beeec517d7

---
+++ component---src-pages-page-3-js-a5ab23bd2d95c10aebd4.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{gWnc:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),l=a.n(n),o=a("Wbzz"),c=a("Bl7J"),r=a("vrFN");t.default=function(){return l.a.createElement(c.a,null,l.a.createElement(r.a,{title:"Page two"}),l.a.createElement("h1",null,"Hi it's for page 3 guys"),l.a.createElement("p",null,"Welcome to page 3"),l.a.createElement(o.Link,{to:"/"},"root `/` is going back of the pages"))}}}]);
+//# sourceMappingURL=component---src-pages-page-3-js-a5ab23bd2d95c10aebd4.js.map
